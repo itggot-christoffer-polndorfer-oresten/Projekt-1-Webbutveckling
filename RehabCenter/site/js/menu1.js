b3f66505635179d87405157d83d6a53927ecb1b5
itggot-christoffer-polndorfer-oresten <christoffer.polndorferoresten@itggot.se>
2018-03-02 13:29:07 +0100

    slidebards (hamburgermenu och profilemenu)

--- RehabCenter/site/js/menu1.js
+++ RehabCenter/site/js/menu1.js
@@ -1,5 +1,12 @@
 function toggleMenu(){
-    var element = document.querySelector(".menu1");
-    element.classList.toggle(".show");
+    var element = document.querySelector(".hamburger_slidemenu");
+    element.classList.toggle("show");
 
 }
+
+function toggleMenu2(){
+    var element = document.querySelector(".profile_menu");
+    element.classList.toggle("show2");
+
+}
+
