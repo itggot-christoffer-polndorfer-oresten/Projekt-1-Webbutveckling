9f08598ee2480a5d34208af51f1e211ff0a788d7
itggot-christoffer-polndorfer-oresten <christoffer.polndorferoresten@itggot.se>
2018-03-23 12:33:28 +0100

    allt fungerar, lade in entill HTML sida

---
+++ RehabCenter/site/public/js/menu1.js
@@ -0,0 +1,12 @@
+function toggleMenu(){
+    var element = document.querySelector(".hamburger_slidemenu");
+    element.classList.toggle("show");
+
+}
+
+function toggleMenu2(){
+    var element = document.querySelector(".profile_menu");
+    element.classList.toggle("show2");
+
+}
+
